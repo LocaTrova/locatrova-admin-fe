c67075bc7734c1e0cae0e1bb2967c505a5c6ab4e
Alessio Persichetti <alessio.persichetti@ectmingegneria.it>
2025-06-27 01:00:57 +0200

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/locatrova-admin-fe/',
   plugins: [react()],
   // server: {
   //   host: true, // Needed for Docker/Render
